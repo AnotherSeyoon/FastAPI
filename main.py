a9de05271bc1565c9070afea35a98352ad5d2405
saaeyun <issou0626@naver.com>
2021-12-14 09:26:48 +0900

    선택적 매개변수

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-from fastapi import FastAPI
+from typing import Optional
 
-fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/items/")
-async def read_item(skip: int = 0, limit: int = 10):
-    return fake_items_db[skip : skip + limit]
+@app.get("/items/{item_id}")
+async def read_item(item_id: str, q: Optional[str] = None):
+    if q:
+        return {"item_id": item_id, "q": q}
+    
+    return {"item_id": item_id}
